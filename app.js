6fcf8885ae6feee372359d2bdec17a09057a1f6d
stromamelie <season@fabrik.haus>
2025-05-27 00:49:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+app.use('/', createProxyMiddleware({
+  target: 'https://beta.atna.xyz/dl/home',
+  changeOrigin: true,
+  onProxyRes: (proxyRes) => {
+    delete proxyRes.headers['x-frame-options'];
+    delete proxyRes.headers['content-security-policy'];
+  }
+}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy listening on port ${PORT}`);
+});
